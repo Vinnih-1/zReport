0b20e2ca89cc67bc906d7ba48e33b940d98dc156
Vinícius Albert <vinifantal@hotmail.com>
2022-03-25 16:04:50 -0300

    Classe criada para gerenciamento dos reports

---
+++ src/main/java/kazumy/plugin/zreport/spigot/report/manager/ReportManager.java
@@ -0,0 +1,38 @@
+package kazumy.plugin.zreport.spigot.report.manager;
+
+import kazumy.plugin.zreport.spigot.MainReport;
+import kazumy.plugin.zreport.spigot.configuration.ReportValue;
+import kazumy.plugin.zreport.spigot.report.Report;
+import lombok.Data;
+import org.bukkit.Bukkit;
+
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Data(staticConstructor = "of")
+public class ReportManager {
+
+    private final MainReport plugin;
+    private final List<Report> currentReport = new ArrayList<>();
+
+    /**
+     * Roda a cada 1 segundo para checar e limpar as denúncias expirados
+     *
+     * @return Retorna uma instância desta classe
+     */
+    public ReportManager run() {
+        Bukkit.getScheduler().runTaskTimerAsynchronously(plugin, () -> {
+            currentReport.stream()
+                    .filter(report -> LocalDateTime.now(ZoneId.of("America/Sao_Paulo")).minusMinutes(ReportValue.get(ReportValue::timeout))
+                            .isAfter(LocalDateTime.parse(report.getData().getDate())))
+                    .collect(Collectors.toSet()).forEach(Report::invalidate);
+        }, 0, 20L * 60);
+
+        return this;
+    }
+
+
+}
