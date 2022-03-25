4863a1c79328118d31742d639f8a71124ec858c9
Vinícius Albert <vinifantal@hotmail.com>
2022-03-25 16:04:31 -0300

    ViewerAdapter para fazer a requisição do objeto `Viewer` do banco de dados

---
+++ src/main/java/kazumy/plugin/zreport/spigot/report/viewer/adapter/ViewerAdapter.java
@@ -0,0 +1,21 @@
+package kazumy.plugin.zreport.spigot.report.viewer.adapter;
+
+import com.henryfabio.sqlprovider.executor.adapter.SQLResultAdapter;
+import com.henryfabio.sqlprovider.executor.result.SimpleResultSet;
+import kazumy.plugin.zreport.spigot.report.reputation.Reputation;
+import kazumy.plugin.zreport.spigot.report.viewer.Viewer;
+import kazumy.plugin.zreport.spigot.report.viewer.impl.ViewerImpl;
+import lombok.val;
+
+public class ViewerAdapter implements SQLResultAdapter<Viewer> {
+
+    @Override
+    public Viewer adaptResult(SimpleResultSet result) {
+        val viewer = new ViewerImpl(result.get("nickname"));
+        viewer.setContributions(result.get("contributions"));
+        viewer.setReputation(Reputation.valueOf(result.get("reputation")));
+        viewer.setId(result.get("id"));
+
+        return viewer;
+    }
+}
