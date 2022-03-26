b8d20afecddc532c910106cdd146137210f7360d
Vinícius Albert <vinifantal@hotmail.com>
2022-03-26 14:38:56 -0300

    Sistema de prioridades adicionada, e ID transformado em String

--- src/main/java/kazumy/plugin/zreport/spigot/report/data/ReportData.java
+++ src/main/java/kazumy/plugin/zreport/spigot/report/data/ReportData.java
@@ -1,15 +1,18 @@
 package kazumy.plugin.zreport.spigot.report.data;
 
-import kazumy.plugin.zreport.spigot.report.type.ReportType;
+import kazumy.plugin.zreport.spigot.report.enums.Priority;
+import kazumy.plugin.zreport.spigot.report.enums.ReportType;
 import lombok.Builder;
 import lombok.Data;
+import lombok.ToString;
 
 @Data
+@ToString
 @Builder(builderClassName = "build")
 public class ReportData {
 
     private boolean approved;
-    private Integer id;
     private ReportType reportType;
-    private String reason, evidence, server, date;
+    private Priority priority;
+    private String id, reason, evidence, server, date;
 }
