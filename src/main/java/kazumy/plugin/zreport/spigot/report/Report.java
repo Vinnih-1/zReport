cdcf1f8262ddd2d00dfa8e7dbbe148bccad344b9
Vinícius Albert <vinifantal@hotmail.com>
2022-03-26 14:37:58 -0300

    Vários métodos adicionados e comentados

--- src/main/java/kazumy/plugin/zreport/spigot/report/Report.java
+++ src/main/java/kazumy/plugin/zreport/spigot/report/Report.java
@@ -3,14 +3,20 @@ package kazumy.plugin.zreport.spigot.report;
 import com.henryfabio.sqlprovider.executor.SQLExecutor;
 import kazumy.plugin.zreport.spigot.MainReport;
 import kazumy.plugin.zreport.spigot.configuration.MessageValue;
+import kazumy.plugin.zreport.spigot.configuration.ReportValue;
 import kazumy.plugin.zreport.spigot.report.data.ReportData;
 import kazumy.plugin.zreport.spigot.report.manager.ReportManager;
 import kazumy.plugin.zreport.spigot.report.viewer.entity.Author;
 import kazumy.plugin.zreport.spigot.report.viewer.entity.User;
 import lombok.Data;
 import lombok.val;
+import net.md_5.bungee.api.chat.ClickEvent;
+import net.md_5.bungee.api.chat.TextComponent;
 import org.bukkit.Bukkit;
 
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.ArrayList;
 import java.util.Optional;
 import java.util.function.Consumer;
 
@@ -31,27 +37,66 @@ public class Report {
         return this;
     }
 
-    public Optional<Report> findReport(ReportManager reportManager) {
-        return reportManager.getCurrentReport().stream().filter(report -> report.getUser().getName().equals(this.getUser().getName())).findAny();
+    /**
+     * Linka o Author da denúncia com a denúncia em um HashMap,
+     * para setagem da evidência posteriormente no evento de chat
+     *
+     * @param reportManager Instância da classe ReportManager
+     * @return A instância desta classe
+     */
+    public Report waitEvidence(ReportManager reportManager) {
+        reportManager.getWaitingEvidence().put(this.getAuthor(), this);
+        return this;
     }
 
+    public Optional<Report> findReport(ReportManager reportManager) {
+        return reportManager.getCurrentReport().stream().filter(report -> report.getUser().getName().equals(this.getUser().getName())).findFirst();
+    }
+
+    /**
+     * Deixa a denúncia pendente para fiscalização da equipe responsável,
+     * e alerta todos que estiverem presente
+     *
+     * @param reportManager Instância da classe ReportManager
+     */
     public void enableReport(ReportManager reportManager) {
         reportManager.getCurrentReport().add(this);
         this.sendToDatabase(MainReport.getInstance().getExecutor());
+
         Bukkit.getOnlinePlayers().stream().filter(player -> player.hasPermission("zreport.seereport"))
                 .forEach(player -> {
-                    val alertMessage = MessageValue.get(MessageValue::reportAlertMessage);
+                    val alertMessage = new ArrayList<>(MessageValue.get(MessageValue::reportAlertMessage));
                     alertMessage.replaceAll(message -> message.replace('&', '§')
                             .replace("%apelido", this.getUser().getName())
                             .replace("%autor", this.getAuthor().getName())
                             .replace("%motivo", this.getData().getReason())
                             .replace("%prova", this.getData().getEvidence())
+                            .replace("%prioridade", this.getData().getPriority().getName())
                             .replace("%vezes", String.valueOf(reportManager.getAllReportsByName(this.getUser().getName()).size())));
                     alertMessage.forEach(message -> player.sendMessage(message));
-//                    val component = new TextComponent("§aClique aqui para teleportar");
-//                    component.setClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/teleport " + this.getUser().getName()));
-//                    player.spigot().sendMessage(component);
+                    val component = new TextComponent("  §aClique §a§naqui§a para visualizar");
+                    component.setClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/reportinfo " + this.getUser().getName()));
+                    player.spigot().sendMessage(component);
                 });
+    }
+
+    /**
+     * Faz a setagem da prova da denúncia
+     *
+     * @param evidence Link da prova dada pelo Author
+     */
+    public void setEvidenceToReport(String evidence) {
+        if (evidence.equalsIgnoreCase("Nenhuma")) {
+            if (!this.getAuthor().toPlayer().hasPermission("zreport.semprova")) {
+                this.getAuthor().toPlayer().sendMessage(MessageValue.get(MessageValue::withoutEvidence));
+                return;
+            }
+            this.getData().setEvidence("Nenhuma");
+        } else this.getData().setEvidence(evidence);
+        this.getData().setDate(LocalDateTime.now(ZoneId.of("America/Sao_Paulo")).toString());
+        this.getData().setServer(ReportValue.get(ReportValue::server));
+        this.getAuthor().toPlayer().sendMessage(MessageValue.get(MessageValue::sendedReport));
+        this.enableReport(MainReport.getInstance().getReportManager());
     }
 
     /**
@@ -73,6 +118,12 @@ public class Report {
         reportManager.getAllReportsByName(this.getUser().getName()).forEach(reportManager.getCurrentReport()::remove);
     }
 
+    /**
+     * Caso a denúncia seja concretizada, e o User punido,
+     * ele altera a coluna "approved" para true
+     *
+     * @param executor Instância da classe SQLExecutor
+     */
     public void approveReportInDatabase(SQLExecutor executor, ReportManager reportManager) {
         reportManager.getAllReportsByName(this.getUser().getName()).forEach(report -> {
             executor.updateQuery("UPDATE denunciations SET approved = ? WHERE nickname = ?", s -> {
@@ -85,18 +136,20 @@ public class Report {
     /**
      * Envia o objeto para o banco de dados
      *
-     * @param executor
+     * @param executor Instância da classe SQLExecutor
      */
     public void sendToDatabase(SQLExecutor executor) {
-        executor.updateQuery("INSERT INTO denunciations(nickname, author, reason, evidence, server, type, approved, date) VALUES(?, ?, ?, ?, ?, ?)", s -> {
-            s.set(1, this.getUser().getName().toLowerCase());
-            s.set(2, this.getAuthor().getName().toLowerCase());
-            s.set(3, this.getData().getReason());
-            s.set(4, this.getData().getEvidence());
-            s.set(5, this.getData().getServer());
-            s.set(6, this.getData().getReportType().toString());
-            s.set(7, this.getData().isApproved());
-            s.set(8, this.getData().getDate());
+        executor.updateQuery("INSERT INTO denunciations(id, nickname, author, reason, evidence, server, type, priority, approved, date) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", s -> {
+            s.set(1, this.getData().getId());
+            s.set(2, this.getUser().getName().toLowerCase());
+            s.set(3, this.getAuthor().getName().toLowerCase());
+            s.set(4, this.getData().getReason());
+            s.set(5, this.getData().getEvidence());
+            s.set(6, this.getData().getServer());
+            s.set(7, this.getData().getReportType().toString());
+            s.set(8, this.getData().getPriority().toString());
+            s.set(9, this.getData().isApproved());
+            s.set(10, this.getData().getDate());
         });
     }
 }
