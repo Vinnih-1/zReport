c9c60f407e21b75c295ac7d75c46ce294be2963c
Vinícius Albert <vinifantal@hotmail.com>
2022-03-26 14:39:37 -0300

    Coletando apenas a última denúncia de um indivíduo

--- src/main/java/kazumy/plugin/zreport/spigot/command/ReportInfoCMD.java
+++ src/main/java/kazumy/plugin/zreport/spigot/command/ReportInfoCMD.java
@@ -3,20 +3,20 @@ package kazumy.plugin.zreport.spigot.command;
 import kazumy.plugin.zreport.spigot.MainReport;
 import kazumy.plugin.zreport.spigot.command.base.BaseCommand;
 import kazumy.plugin.zreport.spigot.configuration.MessageValue;
-import kazumy.plugin.zreport.spigot.report.Report;
 import kazumy.plugin.zreport.spigot.report.menu.ProfileReportInventory;
-import kazumy.plugin.zreport.spigot.report.viewer.entity.Author;
-import kazumy.plugin.zreport.spigot.report.viewer.entity.User;
 import lombok.val;
 import me.saiintbrisson.minecraft.command.annotation.Command;
 import me.saiintbrisson.minecraft.command.command.Context;
 import me.saiintbrisson.minecraft.command.target.CommandTarget;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import java.util.stream.Collectors;
 
 public class ReportInfoCMD extends BaseCommand {
 
     @Override
-    @Command(name = "reportinfo", permission = "zreport.inforeport", target = CommandTarget.PLAYER)
+    @Command(name = "reportinfo", permission = "zreport.seereport", target = CommandTarget.PLAYER)
     public void execute(Context<CommandSender> context) {
         val sender = context.getSender();
 
@@ -24,13 +24,12 @@ public class ReportInfoCMD extends BaseCommand {
             sender.sendMessage("§c§lERRO §fUtilize o comando correto: §c/reportinfo [Jogador]");
             return;
         }
-        val currentReport = new Report().configureReport(report -> {
-            report.setUser(new User(context.getArg(0)));
-            report.setAuthor(new Author(sender.getName()));
-        }).findReport(MainReport.getInstance().getReportManager());
+        val allReportsByName = MainReport.getInstance().getReportManager().getCurrentReport().stream()
+                .filter(report -> report.getUser().getName().equals(context.getArg(0)))
+                .collect(Collectors.toList());
 
-        if (currentReport.isPresent())
-            new ProfileReportInventory(currentReport.get());
+        if (!allReportsByName.isEmpty())
+            new ProfileReportInventory(allReportsByName.get(allReportsByName.size() - 1)).openInventory((Player) sender);
         else sender.sendMessage(MessageValue.get(MessageValue::reportNotFounded));
     }
 }
