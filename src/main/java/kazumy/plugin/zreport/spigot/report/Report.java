37f0eb31354aaa84d8fefeece04228008b346bd4
Vinícius Albert <vinifantal@hotmail.com>
2022-03-26 01:47:44 -0300

    Alterações no banco de dados e nos métodos da classe

--- src/main/java/kazumy/plugin/zreport/spigot/report/Report.java
+++ src/main/java/kazumy/plugin/zreport/spigot/report/Report.java
@@ -1,6 +1,7 @@
 package kazumy.plugin.zreport.spigot.report;
 
 import com.henryfabio.sqlprovider.executor.SQLExecutor;
+import kazumy.plugin.zreport.spigot.MainReport;
 import kazumy.plugin.zreport.spigot.configuration.MessageValue;
 import kazumy.plugin.zreport.spigot.report.data.ReportData;
 import kazumy.plugin.zreport.spigot.report.manager.ReportManager;
@@ -8,8 +9,6 @@ import kazumy.plugin.zreport.spigot.report.viewer.entity.Author;
 import kazumy.plugin.zreport.spigot.report.viewer.entity.User;
 import lombok.Data;
 import lombok.val;
-import net.md_5.bungee.api.chat.ClickEvent;
-import net.md_5.bungee.api.chat.TextComponent;
 import org.bukkit.Bukkit;
 
 import java.util.Optional;
@@ -27,8 +26,9 @@ public class Report {
      *
      * @param consumer
      */
-    public void configure(Consumer<Report> consumer) {
+    public Report configureReport(Consumer<Report> consumer) {
         consumer.accept(this);
+        return this;
     }
 
     public Optional<Report> findReport(ReportManager reportManager) {
@@ -37,6 +37,7 @@ public class Report {
 
     public void enableReport(ReportManager reportManager) {
         reportManager.getCurrentReport().add(this);
+        this.sendToDatabase(MainReport.getInstance().getExecutor());
         Bukkit.getOnlinePlayers().stream().filter(player -> player.hasPermission("zreport.seereport"))
                 .forEach(player -> {
                     val alertMessage = MessageValue.get(MessageValue::reportAlertMessage);
@@ -45,11 +46,11 @@ public class Report {
                             .replace("%autor", this.getAuthor().getName())
                             .replace("%motivo", this.getData().getReason())
                             .replace("%prova", this.getData().getEvidence())
-                            .replace("%vezes", String.valueOf(this.getData().getAmount())));
-                    alertMessage.forEach(player::sendMessage);
-                    val component = new TextComponent("§aClique aqui para teleportar");
-                    component.setClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/teleport " + this.getUser().getName()));
-                    player.spigot().sendMessage(component);
+                            .replace("%vezes", String.valueOf(reportManager.getAllReportsByName(this.getUser().getName()).size())));
+                    alertMessage.forEach(message -> player.sendMessage(message));
+//                    val component = new TextComponent("§aClique aqui para teleportar");
+//                    component.setClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/teleport " + this.getUser().getName()));
+//                    player.spigot().sendMessage(component);
                 });
     }
 
@@ -59,15 +60,8 @@ public class Report {
      *
      * @param executor para enviar o report ao banco de dados
      */
-    public void validade(SQLExecutor executor) {
-        executor.updateQuery("INSERT INTO denunciations(nickname, author, reason, evidence, server, date) VALUES(?, ?, ?, ?, ?, ?)", s -> {
-            s.set(1, this.getUser().getName().toLowerCase());
-            s.set(2, this.getAuthor().getName().toLowerCase());
-            s.set(3, this.getData().getReason());
-            s.set(4, this.getData().getEvidence());
-            s.set(5, this.getData().getServer());
-            s.set(6, this.getData().getDate());
-        });
+    public void validadeReport(SQLExecutor executor) {
+        this.approveReportInDatabase(executor, MainReport.getInstance().getReportManager());
         this.getAuthor().rewardContributor(executor);
     }
 
@@ -75,6 +69,34 @@ public class Report {
      * Executado quando algum staff invalida o report,
      * ou quando o report é expirado pelo tempo.
      */
-    public void invalidate() {
+    public void invalidateReport(ReportManager reportManager) {
+        reportManager.getAllReportsByName(this.getUser().getName()).forEach(reportManager.getCurrentReport()::remove);
+    }
+
+    public void approveReportInDatabase(SQLExecutor executor, ReportManager reportManager) {
+        reportManager.getAllReportsByName(this.getUser().getName()).forEach(report -> {
+            executor.updateQuery("UPDATE denunciations SET approved = ? WHERE nickname = ?", s -> {
+                s.set(1, this.getData().isApproved());
+                s.set(2, this.getUser().getName().toLowerCase());
+            });
+        });
+    }
+
+    /**
+     * Envia o objeto para o banco de dados
+     *
+     * @param executor
+     */
+    public void sendToDatabase(SQLExecutor executor) {
+        executor.updateQuery("INSERT INTO denunciations(nickname, author, reason, evidence, server, type, approved, date) VALUES(?, ?, ?, ?, ?, ?)", s -> {
+            s.set(1, this.getUser().getName().toLowerCase());
+            s.set(2, this.getAuthor().getName().toLowerCase());
+            s.set(3, this.getData().getReason());
+            s.set(4, this.getData().getEvidence());
+            s.set(5, this.getData().getServer());
+            s.set(6, this.getData().getReportType().toString());
+            s.set(7, this.getData().isApproved());
+            s.set(8, this.getData().getDate());
+        });
     }
 }
