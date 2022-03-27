a93a3ac9f111e1c8930e1c7cbd1e1947cccad1b4
Vinícius Albert <vinifantal@hotmail.com>
2022-03-27 15:10:33 -0300

    Método para coletagem de todos as denúncias de um User trocado de lugar

--- src/main/java/kazumy/plugin/zreport/spigot/report/manager/ReportManager.java
+++ src/main/java/kazumy/plugin/zreport/spigot/report/manager/ReportManager.java
@@ -6,10 +6,12 @@ import kazumy.plugin.zreport.spigot.report.Report;
 import kazumy.plugin.zreport.spigot.report.adapter.ReportAdapter;
 import kazumy.plugin.zreport.spigot.report.viewer.entity.Author;
 import lombok.Data;
+import lombok.val;
 import org.bukkit.Bukkit;
 
 import java.time.LocalDateTime;
 import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -30,21 +32,22 @@ public class ReportManager {
      */
     public ReportManager run() {
         Bukkit.getScheduler().runTaskTimerAsynchronously(plugin, () -> {
-            System.out.println(ReportValue.get(ReportValue::timeout));
             currentReport.stream()
                     .filter(report -> LocalDateTime.now(ZoneId.of("America/Sao_Paulo")).minusMinutes(ReportValue.get(ReportValue::timeout))
                             .isAfter(LocalDateTime.parse(report.getData().getDate())))
-                    .collect(Collectors.toSet()).forEach(report -> report.invalidateReport(this));
+                    .collect(Collectors.toSet()).forEach(report -> report.disableReport(this));
         }, 0, 20L);
 
         return this;
     }
 
-    public List<Report> getAllReportsByName(String name) {
-        return currentReport.stream().filter(report -> report.getUser().getName().equals(name)).collect(Collectors.toList());
-    }
-
     public Report getReportById(String id) {
         return plugin.getExecutor().resultOneQuery("SELECT * FROM denunciations WHERE id = ?", s -> s.set(1, id), ReportAdapter.class);
+    }
+
+    public static String formattedDate(String date) {
+        val formatted = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm").format(LocalDateTime.parse(date))
+                .split(" ");
+        return formatted[0] + " às " + formatted[1];
     }
 }
--- src/main/java/kazumy/plugin/zreport/spigot/report/viewer/entity/User.java
+++ src/main/java/kazumy/plugin/zreport/spigot/report/viewer/entity/User.java
@@ -1,10 +1,34 @@
 package kazumy.plugin.zreport.spigot.report.viewer.entity;
 
+import com.henryfabio.sqlprovider.executor.SQLExecutor;
+import kazumy.plugin.zreport.spigot.report.Report;
+import kazumy.plugin.zreport.spigot.report.adapter.ReportAdapter;
+import kazumy.plugin.zreport.spigot.report.manager.ReportManager;
 import kazumy.plugin.zreport.spigot.report.viewer.impl.ViewerImpl;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class User extends ViewerImpl {
 
     public User(String name) {
         super(name);
+    }
+
+    /**
+     * Retorna todos as denúncias que estiverem na memória
+     *
+     * @param reportManager Instância da classe ReportManager para acessar a Lista de denúncias
+     * @return A lista de denúncias que estiverem com o mesmo nome do User
+     */
+    public List<Report> getAllReportsEnabled(ReportManager reportManager) {
+        return reportManager.getCurrentReport().stream().filter(report -> report.getUser().getName().equals(this.getName())).collect(Collectors.toList());
+    }
+
+    public List<Report> getAllReportsStored(SQLExecutor executor) {
+        return new ArrayList<>(executor.resultManyQuery("SELECT * FROM denunciations WHERE nickname = ?", s -> {
+            s.set(1, this.getName().toLowerCase());
+        }, ReportAdapter.class));
     }
 }
