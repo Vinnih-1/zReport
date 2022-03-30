e9c0d46bb18834e9b8a9d5d71827096fe883823c
Vinícius Albert <vinifantal@hotmail.com>
2022-03-30 00:44:12 -0300

    Cor própria adicionada às prioridades da denúncia

--- src/main/java/kazumy/plugin/zreport/spigot/report/enums/Priority.java
+++ src/main/java/kazumy/plugin/zreport/spigot/report/enums/Priority.java
@@ -3,10 +3,13 @@ package kazumy.plugin.zreport.spigot.report.enums;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import java.awt.*;
+
 @AllArgsConstructor
 public enum Priority {
 
-    LOW("§7§lBAIXA"), NORMAL("§e§lNORMAL"), HIGHEST("§c§lALTA");
+    LOW("§7§lBAIXA", Color.GRAY), NORMAL("§e§lNORMAL", Color.YELLOW), HIGHEST("§c§lALTA", Color.RED);
 
     @Getter private final String name;
+    @Getter private final Color color;
 }
